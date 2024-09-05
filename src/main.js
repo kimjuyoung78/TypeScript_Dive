27b2dc31e76b7dd58cf81628d2f8d0e0cedced6f
kimjuyoung99 <jjassb404@naver.com>
2024-09-05 20:12:29 +0900

    실행환경 vite로 변경

--- src/main.js
+++
@@ -1,6 +0,0 @@
-function hello(msg) {
-    console.log("Hello ".concat(msg, "?!"));
-}
-hello('world'); //true
-//hello(123) //error 'number' 형식의 인수는 'string' 형식의 매개 변수에 할당될 수 없습니다.ts(2345)
-//hello(true) //error
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
-function hello(msg: string) {//강한 문법을 적용하자 msg 선언은 에러가 떠서 바로 수정
-    console.log(`Hello ${msg}?!`);
-}
-hello('world'); //true
-//hello(123) //error 'number' 형식의 인수는 'string' 형식의 매개 변수에 할당될 수 없습니다.ts(2345)
-//hello(true) //error
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
 
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
 
-//셋팅 순서
-// npm i -D typescript
-//tsconfig.json 
-//package.json 파일에 scripts 부분 
-//  "scripts": {
-//     "tsc": "tsc src/main.ts"
-//   },
-//로 변경
-// 터미널 명령어 : npm run tsc (해당 ts 파일을 js 파일로 생성해줌)
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
