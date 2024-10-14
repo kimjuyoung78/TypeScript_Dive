43a4d2039aa8222a7773229b67b8306f8726c59b
kimjuyoung99 <jjassb404@naver.com>
2024-10-14 22:41:49 +0900

    타입추론 설명 추가

--- src/타입추론.ts
+++ src/타입추론.ts
@@ -1,4 +1,8 @@
-
+//타입 추론(Inference)
+// '추론'이란, 어떠한 판단을 근러고 삼아 다른 판단을 이끌어 냄.
+// 1) 초기화된 변수
+// 2) 기본값이 지정된 매개변수
+// 3) 반환이 있는 함수
 
 let a = 'Hello'
 a = 123 //'number' 형식은 'string' 형식에 할당할 수 없습니다.
@@ -6,5 +10,6 @@ a = true //'boolean' 형식은 'string' 형식에 할당할 수 없습니다.
 function join(a: string, b: string=''):string{
     return a + b
 }
-join('Hello','World')
+const x = join('Hello','World') // x 가 문자열이라는 것을 설명 안해도 타입 추론을 통해 타입 지정을 할 수 있다.
+//이때 a나 b 둘 중에 하나라도 자료형이 선언이 된 경우에만. 둘 다 없으면 오류가 난다.
 join('Good')
